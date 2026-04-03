50cf21d20c9da70c8eaec0b6038c15eb753291b8
Serikov G. (csharp) <serikov.gen@yandex.ru>
2026-04-03 16:55:38 +0300

    sprint_1 feat: добавлен проект моделей приложения, добавлена модель мероприятия

--- Entities/Models/Event.cs
+++ Entities/Models/Event.cs
@@ -1,11 +1,21 @@
 ﻿namespace Entities.Models;
 
+/// <summary>Модель события - мероприятия</summary>
 public class Event
 {
     public Guid Id { get; init; }
+
+
+    /// <summary>Наименование мероприятия</summary>
     public required string Title { get; set; }
+
+    /// <summary>Дата и время начала мероприятия</summary>
     public required DateTime StartAt { get; set; }
+
+    /// <summary>Дата и время завершения мероприятия</summary>
     public required DateTime EndAt { get; set; }
-    
-    public string? Description { get; set; }
+
+
+    /// <summary>Описание мероприятия</summary>
+    public string? Description { get; set; } 
 }
