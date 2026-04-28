2437c6ebc69854ec16abf01f516acebe1be80241
Serikov G. (csharp) <serikov.gen@yandex.ru>
2026-04-28 22:21:38 +0300

    sprint_2 добавлен класс тестирования сервиса EventServiceTests, определены mock-заглушки для зависимостей сервиса

---
+++ EventBrokerAPI.Tests/EventServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Contracts.Repository;
+using Moq;
+using Repository;
+using Service;
+
+namespace EventBrokerAPI.Tests;
+
+public class EventServiceTests
+{
+    private readonly Mock<IMapper> mockMapper = new();
+    private readonly RepositoryContext mockRepositoryContext = new();
+    private readonly Mock<IRepositoryManager> mockRepositoryManager = new();
+
+    private readonly EventService eventService;
+    private readonly EventRepository eventRepository;
+
+    public EventServiceTests()
+    {
+        eventRepository = new EventRepository(mockRepositoryContext);
+        eventService = new EventService(mockRepositoryManager.Object, mockMapper.Object);
+    }
+}
