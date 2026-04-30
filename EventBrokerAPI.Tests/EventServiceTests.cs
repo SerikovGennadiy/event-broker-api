b1ac99f48ae98d976432495f5cc268094f889a0f
Serikov G. (csharp) <serikov.gen@yandex.ru>
2026-04-30 23:50:07 +0300

    sprint_2 добавлен тест создания события

--- EventBrokerAPI.Tests/EventServiceTests.cs
+++ EventBrokerAPI.Tests/EventServiceTests.cs
@@ -1,23 +1,95 @@
 ﻿using AutoMapper;
 using Contracts.Repository;
+using Contracts.Service;
+using Entities.Domain.Models;
+using Entities.ErrorHandling.Exceptions.Event;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
 using Moq;
-using Repository;
 using Service;
+using Shared.DTO;
 
 namespace EventBrokerAPI.Tests;
-
-public class EventServiceTests
+public class EventServiceFixture : IDisposable
 {
-    private readonly Mock<IMapper> mockMapper = new();
-    private readonly RepositoryContext mockRepositoryContext = new();
-    private readonly Mock<IRepositoryManager> mockRepositoryManager = new();
+    public EventService EventService { get; }
 
-    private readonly EventService eventService;
-    private readonly EventRepository eventRepository;
+    public Mock<IRepositoryManager> RepositoryManagerMock { get; } = new();
+    public Mock<IEventRepository> EventRepositoryMock { get; } = new();
+    public Mock<IMapper> MapperMock { get; } = new();
 
-    public EventServiceTests()
+    public EventServiceFixture()
     {
-        eventRepository = new EventRepository(mockRepositoryContext);
-        eventService = new EventService(mockRepositoryManager.Object, mockMapper.Object);
+        RepositoryManagerMock.Setup(x => x.Event)
+            .Returns(EventRepositoryMock.Object);
+
+        EventService = new EventService(
+            RepositoryManagerMock.Object,
+            MapperMock.Object
+        );
+    }
+
+    public void Dispose()
+    {
+        RepositoryManagerMock.Reset();
+        EventRepositoryMock.Reset();
+        MapperMock.Reset();
+
+        GC.SuppressFinalize(this);
     }
 }
+
+public class EventServiceTests : IClassFixture<EventServiceFixture>
+{
+    private readonly EventServiceFixture _fixture;
+    public EventServiceTests(EventServiceFixture fixture) => _fixture = fixture;
+
+    [Fact]
+    public void CreateEvent_ValidData_ReturnsEvent()
+    {
+        //Arrange
+        var createdEventGuid = Guid.CreateVersion7();
+        var eventDTO = new EventDTO(Id: Guid.Empty,
+                                    Title: "Event: hiking",
+                                    Description: "Info about event",
+                                    StartAt: new(2026, 5, 2),
+                                    EndAt: new(2026, 5, 3));
+
+        var eventEntity = new Event()
+        {
+            Id = Guid.Empty,
+            Title = "Event: hiking",
+            Description = "Info about event",
+            StartAt = new(2026, 5, 2),
+            EndAt = new(2026, 5, 3)
+        };
+
+        var createdEventDTO = eventDTO with { Id = createdEventGuid };
+
+        _fixture.MapperMock.Setup(m => m.Map<Event>(eventDTO)).Returns(eventEntity);
+        _fixture.MapperMock.Setup(m => m.Map<EventDTO>(It.IsAny<Event>())).Returns(createdEventDTO);
+        _fixture.EventRepositoryMock.Setup(repo => repo.CreateEvent(It.IsAny<Event>())).Verifiable();
+
+        // Act
+        var result = _fixture.EventService.CreateEvent(eventDTO);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(createdEventDTO, result);
+        _fixture.RepositoryManagerMock.Verify(rm => rm.Event.CreateEvent(It.IsAny<Event>()), Times.Once());
+    }
+}
+   /// private void ValidateEvent(EventDTO eventDTO)
+    //{
+    //    if (string.IsNullOrEmpty(eventDTO.Title))
+    //        throw new EventNoTitleException();
+
+    //    if (eventDTO.EndAt <= eventDTO.StartAt)
+    //        throw new EventBadDateRangeException();
+    //}
+
+    //var entity = mapper.Map<Event>(eventDTO);
+    //   entity.Id = Guid.CreateVersion7();
+
+    //   repositoryManager.Event.CreateEvent(entity);
+
+    //   return mapper.Map<EventDTO>(entity);
