1f6b865c3edf988c9d66576c6a57da72954ab20f
Serikov G. (csharp) <serikov.gen@yandex.ru>
2026-05-11 13:35:46 +0300

    sprint_3 добавлена модель брони на мероприятие

---
+++ Entities/Domain/Models/Booking.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Domain.Contract;
+
+namespace Entities.Domain.Models;
+
+/// <summary>Текущий статус брони на мероприятие</summary>
+public enum BookingStatus
+{
+    /// <summary>Бронь создана, но не подтверждена</summary>
+    Pending,
+    /// <summary>Бронь подтверждена</summary>
+    Confirmed,
+    /// <summary>Бронь отклонена</summary>
+    Rejected,
+}
+
+/// <summary>Модель брони на мероприятие</summary>
+public class Booking : IdEntity
+{
+    /// <summary>Идентификатор брони</summary>
+    public Guid Id { get; set; }
+    /// <summary>Идентификатор мероприятия</summary>
+    public Guid EventId { get; set; }
+    /// <summary>Статус брони</summary>
+    public BookingStatus Status { get; set; }
+    /// <summary>Дата и время создания брони</summary>
+    public DateTime CreatedAt { get; set; }
+    /// <summary>Дата и время обработки брони</summary>
+    public DateTime? ProcessedAt { get; set; }
+}
