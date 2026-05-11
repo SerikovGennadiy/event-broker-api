65ea4e1edbe86cade62e4b4792e3e52a65da225c
Serikov G. (csharp) <serikov.gen@yandex.ru>
2026-05-11 22:51:38 +0300

    sprint_3 сформирован fixture класс для зависимостей BookingService'а

---
+++ EventBrokerAPI.Tests/BookingService/BookingServiceFixture.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Contracts.Repository;
+using Moq;
+using BookingServiceType = Service.BookingService;
+
+namespace EventBrokerAPI.Tests.BookingService;
+internal class BookingServiceFixture : IDisposable
+{
+    public BookingServiceType EventService { get; }
+
+    public Mock<IRepositoryManager> RepositoryManagerMock { get; } = new();
+    public Mock<IBookingRepository> BookingRepositoryMock { get; } = new();
+    public Mock<IMapper> MapperMock { get; } = new();
+
+    public BookingServiceFixture()
+    {
+        RepositoryManagerMock.Setup(x => x.Booking)
+            .Returns(BookingRepositoryMock.Object);
+
+        EventService = new BookingServiceType(
+            RepositoryManagerMock.Object,
+            MapperMock.Object
+        );
+    }
+
+    public void Dispose()
+    {
+        RepositoryManagerMock.Reset();
+        BookingRepositoryMock.Reset();
+        MapperMock.Reset();
+
+        GC.SuppressFinalize(this);
+    }
+}
