a8b1b248492d0cbec3583f1fac6e78814e4f3010
Serikov G. (csharp) <serikov.gen@yandex.ru>
2026-06-28 00:10:57 +0300

    sprint_6 добавлен специальный класс-фабрика создания контекста данных для ручного формирования скриптов миграции (> dotnet ef migrations|database ...) в design-time (без запуска в runtime).

---
+++ EventBrokerAPI/ContextFactory/AppDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EventBrokerAPI.ContextFactory
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                          .AddJsonFile("appsettings.json")
+                                                          .Build();
+
+            var builder = new DbContextOptionsBuilder<AppDbContext>()
+                .UseNpgsql(connectionString: configuration.GetConnectionString("DeafaultConnection"),
+                           npgsqlOptionsAction: m => m.MigrationsAssembly("EventBrokerAPI"));
+
+            return new AppDbContext(builder.Options);
+        }
+    }
+}
