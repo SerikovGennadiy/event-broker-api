f3d6b273881f9d292b937eda93f985b66cd6047a
Serikov G. (csharp) <serikov.gen@yandex.ru>
2026-07-21 23:28:00 +0300

    sprint_8 обновлен AppDbContext и конфигурации моделей UserConfiguration и BookingConfiguration

---
+++ Infrastructure/Persistence/Configuration/UserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configuration;
+
+internal class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Id)
+               .ValueGeneratedNever();
+
+        builder.Property(u => u.Login)
+               .IsRequired()
+               .HasMaxLength(100);
+
+        builder.HasIndex(u => u.Login)
+               .IsUnique();
+
+        builder.Property(u => u.PasswordHash)
+               .IsRequired()
+               .HasMaxLength(256);
+
+        builder.Property(u => u.Role)
+               .IsRequired()
+               .HasConversion<int>();
+    }
+}
