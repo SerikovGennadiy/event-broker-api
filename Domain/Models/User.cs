fdc7e40dc3686fe05b294787133a19e6c9f2eaa4
Serikov G. (csharp) <serikov.gen@yandex.ru>
2026-07-21 22:49:18 +0300

    sprint_8 добавлена модель пользователя

---
+++ Domain/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Contracts;
+
+namespace Domain.Models;
+
+/// <summary>Сущность пользователя</summary>
+public class User : IdEntity
+{
+    /// <summary>Идентификатор пользователя</summary>
+    public Guid Id { get; init; }
+
+    /// <summary>Логин пользователя</summary>
+    public required string Login { get; init; }
+
+    /// <summary>Хеш пароля</summary>
+    public required string PasswordHash { get; init; }
+
+    /// <summary>Роль пользователя</summary>
+    public Role Role { get; private set; }
+
+    // приватный конструктор для EFCore
+    private User() { }
+
+    public static User Create(string login, string passwordHash, Role role = Role.User)
+    {
+       return new User()
+        {
+            Id = Guid.CreateVersion7(),
+            Login = login,
+            PasswordHash = passwordHash,
+            Role = role
+        };
+    }
+}
+
+public enum Role
+{
+    User,
+    Admin
+}
