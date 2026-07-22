04141d5ca073cc6b8b58c705cf1239e156df3658
Serikov G. (csharp) <serikov.gen@yandex.ru>
2026-07-22 23:57:20 +0300

    sprint_8 параметры jwt токена (секрет, издатель, аудиторию, время жизни) вынесены в appsettings.json, добавлен option-класс на них

---
+++ Domain/Options/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Options;
+
+internal class JwtSettings
+{
+    public string Section { get; set; } = "JwtSettings";
+    public string? ValidIssuer { get; set; }
+    public string? ValidAudience { get; set; }
+    public string? Expires { get; set; }
+}
