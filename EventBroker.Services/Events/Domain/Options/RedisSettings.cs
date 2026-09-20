68ae6403252ec0c4c18224594067823fe3e63ebe
Serikov G. (csharp) <serikov.gen@yandex.ru>
2026-09-20 11:53:04 +0300

    sprint_10 классы настроек Redis и их ключи в appsettings.json

---
+++ EventBroker.Services/Events/Domain/Options/RedisSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Events.Domain.Options;
+
+public class RedisSettings
+{
+    public string Section { get; set; } = "Redis";
+
+    public string EndPoint { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public int ConnectTimeout { get; set; }
+    public int SyncTimeout { get; set; }
+    public bool AbortOnConnectFail { get; set; }
+}
